1f78b4d4f5b1cd49337d6c1d6f7daad186bad985
ekaitzAguilar <ekaitz.aguilarbi@elorrieta-errekamari.com>
2026-01-31 19:27:05 +0100

    cambios backend

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.get('/health', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Servidor en puerto ${PORT}`);
 });
