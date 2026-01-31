3099bfc22a30bb639e7e6aaa3ff7686d3482627c
ekaitzAguilar <ekaitz.aguilarbi@elorrieta-errekamari.com>
2026-01-31 12:47:25 +0100

    railway

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.listen(PORT, '0.0.0.0', () => {
 });
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'mysql.railway.internal',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'wQsCWchcLubUNItkNCVrLNvzeHunMmYq',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'railway',
     port: process.env.DB_PORT || 3306
 
